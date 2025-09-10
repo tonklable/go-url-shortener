978da06cb6d83b188d88c030d81da7d2c3b8ce76
tonklable <tonklalor2544@gmail.com>
2025-09-10 22:59:56 +0900

    Add MongoDB connection

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type request struct {
@@ -35,17 +44,25 @@ func postShorten(c *gin.Context) {
 		return
 	}
 
+	collection := connect()
+
 	fmt.Println(newShortenReq.Url)
+
+	hash := hashUrl(newShortenReq.Url)
 
 	newShortenRes := response{
 		ID:        1,
 		Url:       newShortenReq.Url,
-		Code:      "abc",
+		Code:      hash,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
 
-	fmt.Println(newShortenRes.CreatedAt)
+	result, err := collection.InsertOne(context.TODO(), newShortenRes)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("URL inserted with ID: %s\n", result.InsertedID)
 
 	c.IndentedJSON(http.StatusCreated, newShortenRes)
 
@@ -66,4 +83,30 @@ func getShortenByCode(c *gin.Context) {
 
 	c.IndentedJSON(http.StatusOK, newShortenRes)
 
+}
+
+func hashUrl(url string) string {
+	sha1 := sha1.New()
+	io.WriteString(sha1, url)
+	return hex.EncodeToString(sha1.Sum(nil))
+}
+
+func connect() *mongo.Collection {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	MONGO_URI := os.Getenv("MONGO_URI")
+
+	clientOption := options.Client().ApplyURI(MONGO_URI)
+	client, err := mongo.Connect(context.TODO(), clientOption)
+	if err != nil {
+		panic(err)
+	}
+
+	collection := client.Database("go-url-shortener").Collection("urls")
+
+	fmt.Println("Connected to db")
+	return collection
 }
